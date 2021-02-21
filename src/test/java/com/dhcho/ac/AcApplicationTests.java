a2fb1a6cd135ed1dc847651e11f5db0c7c0b652d
daeho1909 <daeho1901@gmail.com>
2021-02-22 02:38:54 +0900

    firsh commit

---
+++ src/test/java/com/dhcho/ac/AcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhcho.ac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

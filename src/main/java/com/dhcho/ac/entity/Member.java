9e4773c2d7874000e1c97bac4e2cf11cf2bb8de8
daeho1909 <daeho1901@gmail.com>
2021-02-28 19:05:23 +0900

    window commit

--- src/main/java/com/dhcho/ac/entity/Member.java
+++ src/main/java/com/dhcho/ac/entity/Member.java
@@ -3,17 +3,14 @@ package com.dhcho.ac.entity;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter @Setter
 public class Member {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue
     private Long id;
     private String name;
 
